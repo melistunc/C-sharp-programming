e9278f6572648ad5c924751fe89b45c89a36f8df
melis tunç <131777538+melistunc@users.noreply.github.com>
2025-03-18 17:09:33 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; //bu kısımlar kütüphanemizi temsil ediyor. Kütüphane, projede kullanılmak istenilen metot vs gibi verileri getirmemizi sağlar. Çağırdığımızda içlerindeki kodlar bu projeye dahil olur.
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args) // ana fonksiyon
+        {
+            Console.WriteLine("Merhaba Ahmet Ali!"); //ekrana yazdırır.
+            Console.Read(); //ekrana yazılan metin hemen kapanmasın diye mutlaka bunu ekleriz. Yoksa ekran kendi kendine kapanır. BU konsolu oku konsolu bekle anlamındadır.
+
+            // açıklama satırı bu şekilde (tek satırlı açıklamalar için kullanılması uygundur.) veya
+            /* bu şekilde gösterilir bu gösterim birden fazla satır için uygundur.*/
+        }
+    }
+}
