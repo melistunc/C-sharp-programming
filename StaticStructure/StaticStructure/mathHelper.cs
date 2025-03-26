5218fc7549d4a687bb102027a8e7dbf831301da8
melis tunç <131777538+melistunc@users.noreply.github.com>
2025-03-26 21:16:31 +0300

    Add files via upload

---
+++ StaticStructure/StaticStructure/mathHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticStructure
+{
+    // static nedir
+    public static class mathHelper // static class içindeki tüm metotlar static olmak zorundadır.
+    {
+        public static double Square(double number)
+        {
+            return number * number;
+        }
+    }
+}
