fa5690430608f917c81f34c05822c992bbb3ae1d
melis tunç <131777538+melistunc@users.noreply.github.com>
2025-04-02 20:29:07 +0300

    Add files via upload

---
+++ 15- virtualMethod/virtualMethod/Hayvan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace virtualMethod
+{
+    class Hayvan
+    {
+        public virtual void SesCikar() // sanal bir metot oldu. override üstüne yazmak demektir.
+        {
+            Console.WriteLine("Hayvan ses çıkarıyor.");
+        }
+    }
+}
