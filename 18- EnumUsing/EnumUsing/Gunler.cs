c38857c0d702309a5f15ede774c3d28836beb462
melis tunç <131777538+melistunc@users.noreply.github.com>
2025-04-02 20:30:47 +0300

    Add files via upload

---
+++ 18- EnumUsing/EnumUsing/Gunler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumUsing
+{
+    public enum Gunler
+    {
+        /*public string Gun { get; set; } // get set olduğu için program.cs'ten veri değişken girişi yapabiliyoruz.*/
+        Pazartesi,
+        Sali,
+        Carsamba,
+        Persembe,
+        Cuma,
+        Cumartesi,
+        Pazar // class'ı silip burayı enum yaptık yani böylece Gunler'i çağırdığımız zaman otomatikmen sadece buradaki değişkenlerden vermek zorunda olacağız.
+    }
+}
---
+++ 18- EnumUsing/EnumUsing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumUsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // enum, oluşturduğumuz sabit değerlerdir.
+
+            Gunler gunler = Gunler.Cumartesi; // burada sadece Gunler enum içinde tanımladıklarımızı yazabiliyoruz. Gunler için kullanacağımız değerleri sabit tuttuk.
+
+            Console.WriteLine("Bugün günlerden "+gunler);
+            Console.Read();
+        }
+    }
+}
