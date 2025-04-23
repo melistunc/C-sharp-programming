f7c56cfcf22bb2a9930dd56ac86c89705383d47a
melis tunç <131777538+melistunc@users.noreply.github.com>
2025-04-23 11:00:10 +0300

    Add files via upload

---
+++ 37-WinformCalculatingRectangleAreaAndPerimeter/37-WinformCalculatingRectangleAreaAndPerimeter/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace _37_WinformCalculatingRectangleAreaAndPerimeter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(113, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kısa kenar:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(115, 223);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(190, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Uzun kenar:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(311, 122);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(299, 44);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(311, 223);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(299, 44);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(115, 460);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 37);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Çevre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(133, 534);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 37);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "--";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(455, 460);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 37);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Alan";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(481, 534);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 37);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "--";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(260, 334);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(459, 56);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 648);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 37-WinformCalculatingRectangleAreaAndPerimeter/37-WinformCalculatingRectangleAreaAndPerimeter/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _37_WinformCalculatingRectangleAreaAndPerimeter
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int kisakenar = int.Parse(textBox1.Text);
+            int uzunkenar = int.Parse(textBox2.Text);
+
+            double cevre = CevreHesapla(kisakenar, uzunkenar);
+            double alan = AlanGesapla(kisakenar,uzunkenar);
+
+            label4.Text = cevre.ToString();
+            label6.Text = alan.ToString();
+        }
+
+        private double CevreHesapla (double kisakenar, double uzunkenar)
+        {
+            return 2 * (kisakenar + uzunkenar);
+        }
+
+        private double AlanGesapla(double kisakenar, double uzunkenar)
+        {
+            return kisakenar * uzunkenar;
+        }
+    }
+}
