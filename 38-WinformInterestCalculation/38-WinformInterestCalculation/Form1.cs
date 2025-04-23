be33a637934762039bcdc99376f0a9a7991df63f
melis tunç <131777538+melistunc@users.noreply.github.com>
2025-04-23 11:11:14 +0300

    Add files via upload

---
+++ 38-WinformInterestCalculation/38-WinformInterestCalculation/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _38_WinformInterestCalculation
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double anapara = Convert.ToDouble(textBox1.Text);
+            double faizorani = Convert.ToDouble(textBox2.Text) / 100; // yüzdelik olarak vermesi için 100'e böldük.
+            int sure = int.Parse(textBox3.Text);
+
+            double toplamfaiz = FaizHesapla(anapara, faizorani, sure);
+            double odenecektutar = anapara + toplamfaiz;
+
+            label5.Text = toplamfaiz.ToString();
+            label7.Text = odenecektutar.ToString();
+        }
+        private double FaizHesapla (double anapara, double faizorani, int sure)
+        {
+            return anapara * faizorani * sure;
+        }
+    }
+}
---
+++ 38-WinformInterestCalculation/38-WinformInterestCalculation/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("38-WinformInterestCalculation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("38-WinformInterestCalculation")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("94e9aabe-a04e-48c8-8fc1-acf5385772f5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
