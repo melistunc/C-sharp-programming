54e672f1ab35a59ace3617bf0dac191647e248fc
melis tunç <131777538+melistunc@users.noreply.github.com>
2025-04-23 10:46:46 +0300

    Add files via upload

---
+++ 36-WiinformMidtermFinalCalculation/36-WiinformMidtermFinalCalculation/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _36_WiinformMidtermFinalCalculation
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double vize = Convert.ToDouble(textBox1.Text);
+            double final = Convert.ToDouble(textBox2.Text);
+
+            double ortalama = 0;
+
+            ortalama = (vize *0.40) + (final * 0.60);
+
+            label4.Text = ortalama.ToString();
+        }
+    }
+}
