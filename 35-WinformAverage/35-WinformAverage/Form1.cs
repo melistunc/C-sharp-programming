4c056e75204fc3843a385bdd43533fa79c25962b
melis tunç <131777538+melistunc@users.noreply.github.com>
2025-04-23 10:22:48 +0300

    Add files via upload

---
+++ 35-WinformAverage/35-WinformAverage/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _35_WinformAverage
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int ders1 = int.Parse(textBox1.Text);
+            int ders2 = int.Parse(textBox2.Text);
+
+            int ortalama = 0;
+
+            ortalama = (ders1 + ders2) / 2;
+
+            label4.Text = ortalama.ToString();
+        }
+    }
+}
