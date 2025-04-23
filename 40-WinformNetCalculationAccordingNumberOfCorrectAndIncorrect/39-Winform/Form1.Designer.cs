2dc7b9b8fd9f2efc0624385820520fb7bd629a52
melis tunç <131777538+melistunc@users.noreply.github.com>
2025-04-23 11:33:51 +0300

    Add files via upload

---
+++ 40-WinformNetCalculationAccordingNumberOfCorrectAndIncorrect/39-Winform/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace _39_Winform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(591, 243);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.ReadOnly = true;
+            this.textBox9.Size = new System.Drawing.Size(88, 44);
+            this.textBox9.TabIndex = 33;
+         
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(591, 175);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(88, 44);
+            this.textBox8.TabIndex = 32;
+            
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(591, 103);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(88, 44);
+            this.textBox7.TabIndex = 31;
+            
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(599, 31);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(67, 37);
+            this.label8.TabIndex = 30;
+            this.label8.Text = "Net";
+           
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(466, 31);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(105, 37);
+            this.label7.TabIndex = 29;
+            this.label7.Text = "Yanlış";
+           
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(345, 31);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(105, 37);
+            this.label6.TabIndex = 28;
+            this.label6.Text = "Doğru";
+            
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(473, 175);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(88, 44);
+            this.textBox6.TabIndex = 27;
+           
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(473, 243);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(88, 44);
+            this.textBox5.TabIndex = 26;
+            
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(473, 103);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(88, 44);
+            this.textBox4.TabIndex = 25;
+            
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(362, 243);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(88, 44);
+            this.textBox3.TabIndex = 24;
+          
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(430, 362);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 57);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(122, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 37);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Biyoloji";
+            
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(362, 175);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(88, 44);
+            this.textBox2.TabIndex = 21;
+          
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(362, 103);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(88, 44);
+            this.textBox1.TabIndex = 20;
+           
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(122, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 37);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Türkçe";
+            
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(122, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 37);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Matematik";
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 40-WinformNetCalculationAccordingNumberOfCorrectAndIncorrect/39-Winform/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _39_Winform
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double matdogru, matyanlis, matnet;
+            double turkcedogru, turkceyanlis, turkcenet;
+            double biyodogru, biyoyanlis, biyonet;
+
+            matdogru = Convert.ToDouble(textBox1.Text);
+            matyanlis = Convert.ToDouble(textBox4.Text);
+
+            turkcedogru = Convert.ToDouble(textBox2.Text);
+            turkceyanlis = Convert.ToDouble(textBox6.Text);
+
+            biyodogru = Convert.ToDouble(textBox3.Text);
+            biyoyanlis = Convert.ToDouble(textBox5.Text);
+
+            matnet = matdogru - (matyanlis / 4);
+            turkcenet = turkcedogru - (turkceyanlis / 4);
+            biyonet = biyodogru - (biyoyanlis / 4);
+
+            textBox7.Text = matnet.ToString();
+            textBox8.Text = turkcenet.ToString();
+            textBox9.Text = biyonet.ToString();
+        }
+    }
+}
