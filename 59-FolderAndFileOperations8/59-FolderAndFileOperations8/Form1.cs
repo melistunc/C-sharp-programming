c7dc0ccadd34744a7640f2afa214d530eb60d772
melis tunç <131777538+melistunc@users.noreply.github.com>
2025-05-12 19:23:32 +0300

    Add files via upload

---
+++ 59-FolderAndFileOperations8/59-FolderAndFileOperations8/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _59_FolderAndFileOperations8
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // metin belgesi (txt)'yi açma.
+            // openFileDialog ile bir belge, dosyayı açarız.
+                openFileDialog1.ShowDialog(); // dosya klasör kısmını açarız.
+            
+                StreamReader sr = new StreamReader(openFileDialog1.FileName);// reader ile bir şeyi oku deriz. nesne oluşturuyoruz class'tan. openfiledialog'tan seçtiğim filename dosyasını oku dedik. okuma işlemi satır satır yapılır.
+                string satir = sr.ReadLine(); // dosyanın her satırını oku ve bunu satir değişkenine ata.
+
+                while (satir != null) // satir bitmeyene kadar ekle listbox'a bu verileri.
+                {
+                    listBox1.Items.Add(satir);
+                    satir = sr.ReadLine(); // satiri okumaya devam et.
+                }
+            }
+        }
+    }
+
